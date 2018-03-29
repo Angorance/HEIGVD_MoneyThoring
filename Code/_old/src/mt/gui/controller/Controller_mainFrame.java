59987d10a4b19afbd4eb5b1f98b5131c5a936744
François <francois.burgener@hotmail.fr>
2018-03-29 10:52:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- Code/_old/src/mt/gui/controller/Controller_mainFrame.java
+++ Code/_old/src/mt/gui/controller/Controller_mainFrame.java
@@ -6,6 +6,7 @@ import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
@@ -25,12 +26,15 @@ public class Controller_mainFrame implements Initializable {
     @FXML
     private JFXDrawer drawer;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         VBox box = null;
         try {
             box = FXMLLoader.load(getClass().getResource("../view/lateralMenu.fxml"));
             drawer.setSidePane(box);
+
+            load(box);
 
             HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(burgerBtn);
             transition.setRate(-1);
@@ -56,6 +60,38 @@ public class Controller_mainFrame implements Initializable {
         selected.getStyleClass().remove("Selected");
         selected = (Button)event.getSource();
         selected.getStyleClass().add("Selected");
+    }
+
+    public void load(VBox box){
+        for(Node node : box.getChildren()){
+            node.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
+                try {
+                    switch (node.getAccessibleText()){
+                    case "Dashboard" :
+                        break;
+                    case "Budget" :
+                        break;
+                    case "Transaction" :
+                        break;
+                    case "Dept" :
+                        break;
+                    case "BankAccount" :
+                        AnchorPane pane = FXMLLoader.load(getClass().getResource("../view/bankAccount.fxml"));
+                        pane.prefWidthProperty().bind(mainContent.widthProperty());
+                        pane.prefHeightProperty().bind(mainContent.heightProperty());
+                        mainContent.getChildren().add(pane);
+                        for(Node r : mainContent.getChildren()){
+                            System.out.println(r.getId());
+                        }
+                        break;
+                    case "Category" :
+                        break;
+                }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            });
+        }
     }
 
 
