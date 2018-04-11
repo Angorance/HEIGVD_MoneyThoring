4263c98d8540a1e2e7c164130fbe2084d7a372ae
Daniel Gonzalez Lopez <idaniel.gonzalezlopezi@gmail.com>
2018-04-11 14:45:21 +0200

    Added sample on ressources

--- Code/mvn_project/src/main/java/bll/User.java
+++ Code/mvn_project/src/main/java/bll/User.java
@@ -9,8 +9,48 @@ public class User {
 
     private String email;
     private String username;
+    private String password;
 
     private boolean isActivated;
 
+    private User() {
 
+    }
+
+    // SETTERS
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // GETTERS
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    // SUPPRESSORS
+
+    public void supp() {
+        // TODO - Suppress the account !
+    }
+
+
+    // INNER CLASS
+
+    public class Connect {
+
+    }
 }
