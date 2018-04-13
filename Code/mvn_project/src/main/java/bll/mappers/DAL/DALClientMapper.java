9e8a8904d20b7ce9cd5e3576ef63ec0b226a61ae
GZeed <guillaume.zaretti@heig-vd.ch>
2018-04-13 14:49:13 +0200

    debut test dal

--- Code/mvn_project/src/main/java/bll/mappers/DAL/DALClientMapper.java
+++ Code/mvn_project/src/main/java/bll/mappers/DAL/DALClientMapper.java
@@ -2,25 +2,47 @@ package bll.mappers.DAL;
 
 import bll.logic.ClientLogic;
 import bll.model.ClientModel;
+import dal.entities.derby.ClientDeEntity;
 import dal.entities.pgsql.ClientPGEntity;
 import dal.ientites.IDALClientEntity;
 
 public class DALClientMapper {
-    public  static IDALClientEntity toDboPG(ClientLogic bo){
+    public static IDALClientEntity toDboPG(ClientModel bo) {
         ClientPGEntity dboPg = new ClientPGEntity();
         dboPg.setId(bo.getId());
         dboPg.setUsername(bo.getUsername());
         dboPg.setEmail(bo.getEmail());
-        dboPg.setPassword("");
-        dboPg.setIsactivated(true);
-        dboPg.setActivationkey("");
-        dboPg.setSalt("");
+        dboPg.setPassword(bo.getPassword());
+        dboPg.setIsactivated(bo.getIsActivated());
+        dboPg.setActivationkey(bo.getKey());
+        dboPg.setSalt(bo.getSalt());
         return dboPg;
     }
-    public  static IDALClientEntity toDboDe(){return null;}
-    public  static ClientModel toBo(IDALClientEntity client){
-       // ClientModel bo = new ClientModel();
-        return null;}
+
+    public static IDALClientEntity toDboDe(ClientModel bo) {
+        ClientDeEntity dboDe = new ClientDeEntity();
+        dboDe.setId(bo.getId());
+        dboDe.setUsername(bo.getUsername());
+        dboDe.setEmail(bo.getEmail());
+        dboDe.setPassword(bo.getPassword());
+        dboDe.setIsactivated(bo.getIsActivated());
+        dboDe.setActivationkey(bo.getKey());
+        dboDe.setSalt(bo.getSalt());
+        return dboDe;
+    }
+
+    public static ClientModel toBo(IDALClientEntity dbo) {
+        // ClientModel bo = new ClientModel();
+        ClientModel bo = new ClientModel();
+        bo.setId(dbo.getId());
+        bo.setUsername(dbo.getUsername());
+        bo.setEmail(dbo.getEmail());
+        bo.setPassword(dbo.getPassword());
+        bo.setIsActivated(dbo.isIsactivated());
+        bo.setKey(dbo.getActivationkey());
+        bo.setSalt(dbo.getSalt());
+        return bo;
+    }
 }
 
 
