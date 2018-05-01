650e53fea0b7e8c9ca28d3e7d3e8b142a6d92d54
Daniel Gonzalez Lopez <idaniel.gonzalezlopezi@gmail.com>
2018-05-02 00:59:15 +0200

    Cleaned BankAccountModel.java, CategoryModel.java, ClientModel.java, IOTransactionModel.java. Updated gitignore

--- Code/mvn_project/src/main/java/bll/model/ClientModel.java
+++ Code/mvn_project/src/main/java/bll/model/ClientModel.java
@@ -1,10 +1,8 @@
 package bll.model;
 
-import bll.logic.ClientLogic;
 import bll.mappers.DAL.DALClientMapper;
 import dal.irepositories.IClientRepository;
 import dal.orm.IORM;
-import dal.orm.PgORM;
 
 /**
  * ClientModel class.
@@ -12,179 +10,212 @@ import dal.orm.PgORM;
  * Only implements the constructors, the getters and the setters.
  *
  * @author Daniel Gonzalez Lopez
- * @version 1.0
+ * @version 1.2
  */
 public class ClientModel {
-
-    private int id;
-
-    private String email;
-    private String username;
-    private String password;
-    private String salt;
-    private String key;
-
-    // Flag to know if the email was verified.
-    private boolean isActivated;
-
-
-    // -------------------------------------------------------------------------
-    // GETTERS -----------------------------------------------------------------
-
-    /**
-     * Get the email of the client.
-     *
-     * @return String containing the email.
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Get the username of the client.
-     *
-     * @return String containing the username.
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * Get the password of the client.
-     *
-     * @return String containing the password.
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * Get the ID of the client.
-     *
-     * @return Int containing the ID.
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * Get the salt used for the password's hash.
-     *
-     * @return Salt used to hash client's password.
-     */
-    public String getSalt() {
-        return salt;
-    }
-
-    /**
-     * Get the validation key generated to validate email.
-     *
-     * @return Key for email validation.
-     */
-    public String getKey() {
-        return key;
-    }
-
-    /**
-     * Get the activated account flag. If this flag is true, it means the email
-     * address of the client was verified.
-     *
-     * @return Activated account flag.
-     */
-    public boolean getIsActivated() {
-        return isActivated;
-    }
-
-
-    // -------------------------------------------------------------------------
-    // SETTERS -----------------------------------------------------------------
-
-    /**
-     * Change the email by the given parameter.
-     *
-     * @param email New email to set.
-     */
-    public void setEmail (String email) {
-        this.email = email;
-    }
-
-    /**
-     * Change the username by the given parameter.
-     *
-     * @param username New username to set.
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * Change the password by the given parameter.
-     *
-     * @param password New password to set.
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * Set the id of the client.
-     *
-     * @param id ID recovered from the database.
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * Change the isActivated flag to true.
-     */
-    public void setActivated(boolean newFlag) {
-        isActivated = newFlag;
-    }
-
-    /**
-     * Set the salt for the password's hash.
-     * Can be set only one time.
-     *
-     * @param salt Salt generated to hash the password.
-     */
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
-
-    /**
-     * Change the key for the email's validation.
-     * Key changed for each new email needing validation.
-     *
-     * @param key Key generated to validate the email.
-     */
-    public void setKey(String key) {
-        this.key = key;
-    }
-    
-    /**
-     * TODO
-     * @param orm
-     */
-    protected void createUser(IORM orm) {
-        try {
-            orm.beginTransaction();
-            orm.getClientRepository().addClient(DALClientMapper.toDboPG(this));
-	        orm.commit();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    
-    protected void updateUser(IORM orm) {
-    	try {
-    		orm.beginTransaction();
-    		orm.getClientRepository().update(DALClientMapper.toDboPG(this));
-    		orm.commit();
-    		
-	    } catch (Exception e) {
-		    System.out.println(e);
-	    }
-    }
+	
+	private int id;
+	
+	private String email;
+	private String username;
+	private String password;
+	private String salt;
+	private String key;
+	
+	// Flag to know if the email was verified.
+	private boolean isActivated;
+	
+	
+	public ClientModel() {}
+	
+	/**
+	 * Create the user entry into the database.
+	 *
+	 * @param orm ORM instance to use.
+	 */
+	protected void createUser(IORM orm) {
+		
+		try {
+			
+			orm.beginTransaction();
+			
+			IClientRepository repo = orm.getClientRepository();
+			repo.addClient(DALClientMapper.toDboPG(this));
+			
+			orm.commit();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 * Update user entry with the new user data.
+	 *
+	 * @param orm ORM instance to use.
+	 */
+	protected void updateUser(IORM orm) {
+		
+		try {
+			
+			orm.beginTransaction();
+			
+			IClientRepository repo = orm.getClientRepository();
+			repo.update(DALClientMapper.toDboPG(this));
+			
+			orm.commit();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+	// -------------------------------------------------------------------------
+	// GETTERS -----------------------------------------------------------------
+	
+	/**
+	 * Get the email of the client.
+	 *
+	 * @return String containing the email.
+	 */
+	public String getEmail() {
+		
+		return email;
+	}
+	
+	/**
+	 * Get the username of the client.
+	 *
+	 * @return String containing the username.
+	 */
+	public String getUsername() {
+		
+		return username;
+	}
+	
+	/**
+	 * Get the password of the client.
+	 *
+	 * @return String containing the password.
+	 */
+	public String getPassword() {
+		
+		return password;
+	}
+	
+	/**
+	 * Get the ID of the client.
+	 *
+	 * @return Int containing the ID.
+	 */
+	public int getId() {
+		
+		return id;
+	}
+	
+	/**
+	 * Get the salt used for the password's hash.
+	 *
+	 * @return Salt used to hash client's password.
+	 */
+	public String getSalt() {
+		
+		return salt;
+	}
+	
+	/**
+	 * Get the validation key generated to validate email.
+	 *
+	 * @return Key for email validation.
+	 */
+	public String getKey() {
+		
+		return key;
+	}
+	
+	/**
+	 * Get the activated account flag. If this flag is true, it means the email
+	 * address of the client was verified.
+	 *
+	 * @return Activated account flag.
+	 */
+	public boolean getIsActivated() {
+		
+		return isActivated;
+	}
+	
+	
+	// -------------------------------------------------------------------------
+	// SETTERS -----------------------------------------------------------------
+	
+	/**
+	 * Change the email by the given parameter.
+	 *
+	 * @param email New email to set.
+	 */
+	public void setEmail(String email) {
+		
+		this.email = email;
+	}
+	
+	/**
+	 * Change the username by the given parameter.
+	 *
+	 * @param username New username to set.
+	 */
+	public void setUsername(String username) {
+		
+		this.username = username;
+	}
+	
+	/**
+	 * Change the password by the given parameter.
+	 *
+	 * @param password New password to set.
+	 */
+	public void setPassword(String password) {
+		
+		this.password = password;
+	}
+	
+	/**
+	 * Set the id of the client.
+	 *
+	 * @param id ID recovered from the database.
+	 */
+	public void setId(int id) {
+		
+		this.id = id;
+	}
+	
+	/**
+	 * Change the isActivated flag to true.
+	 */
+	public void setActivated(boolean newFlag) {
+		
+		isActivated = newFlag;
+	}
+	
+	/**
+	 * Set the salt for the password's hash.
+	 * Can be set only one time.
+	 *
+	 * @param salt Salt generated to hash the password.
+	 */
+	public void setSalt(String salt) {
+		
+		this.salt = salt;
+	}
+	
+	/**
+	 * Change the key for the email's validation.
+	 * Key changed for each new email needing validation.
+	 *
+	 * @param key Key generated to validate the email.
+	 */
+	public void setKey(String key) {
+		
+		this.key = key;
+	}
 }
--- Code/mvn_project/src/main/java/dal/orm/IORM.java
+++ Code/mvn_project/src/main/java/dal/orm/IORM.java
@@ -3,77 +3,80 @@ package dal.orm;
 import dal.dalexception.DALException;
 import dal.irepositories.*;
 
+/**
+ * TODO
+ */
 public interface IORM {
-    /**
-     * Construct an single instance of clientrepositoriy and return it
-     *
-     * @return an instance of IClientRepository
-     */
-    IClientRepository getClientRepository();
-
-    /**
-     * Construct an single instance of clientrepositoriy and return it
-     *
-     * @return an instance of IBankaccountRepository
-     */
-    IBankaccountRepository getBankaccountRepository();
-
-    /**
-     * Construct an single instance of categoryrepostiory and return it
-     *
-     * @return an instance of ICategoryRepository
-     */
-    ICategoryRepository getCategoryRepository();
-
-    /**
-     * Construct an single instance of budgetrepostiory and return it
-     *
-     * @return an instance of IBudgetRepository
-     */
-    IBudgetRepository getBudgetRepository();
-
-    /**
-     * Construct an single instance of dbbtRepository and return it
-     *
-     * @return an instance of IDebtRepository
-     */
-    IDebtRepository getDebtRepository();
-
-    /**
-     * Construct an single instance of clientrepositoriy and return it
-     *
-     * @return an instance of IClientRepository
-     */
-    IRecurrenceRepository getRecurrenceRepository();
-
-    /**
-     * begin a transaction shared bitween all IIotransactionRepository
-     *
-     * @throws DALException
-     */
-    IIotransactionRepository getIotransactionRepository();
-
-    /**
-     * begin a transaction shared bitween all repositories
-     *
-     * @throws DALException
-     */
-    void beginTransaction() throws DALException;
-
-
-    /**
-     * rollback transaction shared bitween all repositories
-     *
-     * @throws DALException
-     */
-    void rollback() throws DALException;
-
-    /**
-     * commit transaction shared bitween all repositories
-     *
-     * @throws DALException
-     */
-    void commit() throws DALException;
-
-
+	
+	/**
+	 * Construct an single instance of clientrepositoriy and return it
+	 *
+	 * @return an instance of IClientRepository
+	 */
+	IClientRepository getClientRepository();
+	
+	/**
+	 * Construct an single instance of clientrepositoriy and return it
+	 *
+	 * @return an instance of IBankaccountRepository
+	 */
+	IBankaccountRepository getBankaccountRepository();
+	
+	/**
+	 * Construct an single instance of categoryrepostiory and return it
+	 *
+	 * @return an instance of ICategoryRepository
+	 */
+	ICategoryRepository getCategoryRepository();
+	
+	/**
+	 * Construct an single instance of budgetrepostiory and return it
+	 *
+	 * @return an instance of IBudgetRepository
+	 */
+	IBudgetRepository getBudgetRepository();
+	
+	/**
+	 * Construct an single instance of dbbtRepository and return it
+	 *
+	 * @return an instance of IDebtRepository
+	 */
+	IDebtRepository getDebtRepository();
+	
+	/**
+	 * Construct an single instance of clientrepositoriy and return it
+	 *
+	 * @return an instance of IClientRepository
+	 */
+	IRecurrenceRepository getRecurrenceRepository();
+	
+	/**
+	 * begin a transaction shared bitween all IIotransactionRepository
+	 *
+	 * @throws DALException
+	 */
+	IIotransactionRepository getIotransactionRepository();
+	
+	/**
+	 * begin a transaction shared bitween all repositories
+	 *
+	 * @throws DALException
+	 */
+	void beginTransaction() throws DALException;
+	
+	
+	/**
+	 * rollback transaction shared bitween all repositories
+	 *
+	 * @throws DALException
+	 */
+	void rollback() throws DALException;
+	
+	/**
+	 * commit transaction shared bitween all repositories
+	 *
+	 * @throws DALException
+	 */
+	void commit() throws DALException;
+	
 }
