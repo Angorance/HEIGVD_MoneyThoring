a88709c48417667a0ee6dfa465678b7dbb56908e
François <francois.burgener@hotmail.fr>
2018-05-10 21:49:07 +0200

    quelque modification sur le graphique en courbe dashboard/CB

--- Code/mvn_project/src/main/java/gui/controller/Controller_dashboard.java
+++ Code/mvn_project/src/main/java/gui/controller/Controller_dashboard.java
@@ -1,20 +1,29 @@
 package gui.controller;
 
+import bll.logic.BankAccountLogic;
+import bll.logic.ClientLogic;
 import bll.logic.IOTransactionLogic;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXNodesList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
 
 /**
@@ -46,7 +55,8 @@ public class Controller_dashboard implements IController, Initializable {
 		private final String incomeColor = "#8ce589";
 		private final String outgoColor = "#f2a7a8";
 		
-		transactionDisplayer(IOTransactionLogic t){
+		transactionDisplayer(IOTransactionLogic t) {
+			
 			transaction = t;
 			
 			lblDate = new Label(transaction.getDate().toString());
@@ -59,11 +69,11 @@ public class Controller_dashboard implements IController, Initializable {
 			paneDisplay.getChildren().add(lblPrix);
 			
 			
-			paneDisplay.setConstraints(lblDate, 0,0,0,0,HPos.LEFT, VPos.CENTER);
-			paneDisplay.setConstraints(lblCaption, 1,0,0,0,HPos.CENTER, VPos.CENTER);
-			paneDisplay.setConstraints(lblPrix, 2,0,0,0,HPos.RIGHT, VPos.CENTER);
+			paneDisplay.setConstraints(lblDate, 0, 0, 0, 0, HPos.LEFT, VPos.CENTER);
+			paneDisplay.setConstraints(lblCaption, 1, 0, 0, 0, HPos.CENTER, VPos.CENTER);
+			paneDisplay.setConstraints(lblPrix, 2, 0, 0, 0, HPos.RIGHT, VPos.CENTER);
 			
-			if(transaction.isIncome()) {
+			if (transaction.isIncome()) {
 				paneDisplay.setStyle("-fx-background-radius: 10px; -fx-background-color: " + incomeColor + ";");
 			} else {
 				paneDisplay.setStyle("-fx-background-radius: 10px; -fx-background-color: " + outgoColor + ";");
@@ -73,12 +83,41 @@ public class Controller_dashboard implements IController, Initializable {
 		}
 	}
 	
-	@Override public void createItem(Object result) {
-		// TODO création de la transaction
+	private void callForm(boolean isIncome) {
+		
+		/* we load the form fxml*/
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/view/formTransaction.fxml"));
+		
+		/*Create a instance of the controller of bank account form*/
+		Controller_formTransaction controller = new Controller_formTransaction(this, isIncome, null);
+		
+		/*Sets the controller associated with the root object*/
+		loader.setController(controller);
+		
+		paneForm.setVisible(true);
+		paneForm.setMouseTransparent(false);
+		
+		try {
+			
+			AnchorPane pane = loader.load();
+			paneForm.getChildren().add(pane);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
 	
-	private void callForm(IOTransactionLogic t){
-		 // TODO loader le formulaire dans paneForm (s'inspirer des autrees vues)
+	private void unloadform() {
+		
+		paneForm.getChildren().clear();
+		paneForm.setMouseTransparent(true);
+		paneForm.setVisible(false);
+	}
+	@Override public void createItem(Object result) {
+		unloadform();
+		if(result != null){
+			// TODO création du transactionDisplayer
+		}
 	}
 	
 	@Override public void deleteItem(Object toDelete) {
@@ -90,41 +129,99 @@ public class Controller_dashboard implements IController, Initializable {
 	}
 	
 	@Override public void initialize(URL location, ResourceBundle resources) {
+		
 		paneForm.setVisible(false);
 		paneForm.setMouseTransparent(true);
-		btnAddTransaction.setOnAction(event -> callForm(null));
 		
-		//TODO CREATION DE TRANSACTION (set les onAction de btnIncome et btnOutgo) (françois, peut-être ?)
 		initNodeListe();
 		
+		btnOutgo.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override public void handle(ActionEvent event) {
+				
+				callForm(false);
+			}
+		});
+		
+		btnIncome.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override public void handle(ActionEvent event) {
+				callForm(true);
+			}
+		});
+		
 		// TODO line chart : progression du/des comptes bancaire pour les 30 derniers jours
+		setDataLineChart();
 		
 		// TODO pie chart : répartition des dépenses des 30 derniers jours par catégories
 		
 		// TODO lister les 30 dernière transaction dans le paneList (juste créer les displayer)
 		
 		
+	}
 	
+	private void setDataLineChart() {
+		
+		BankAccountLogic bal = new BankAccountLogic();
+		
+		for (BankAccountLogic b : ClientLogic.getInstance().getBankAccounts()) {
+			if (b.isDefault()) {
+				bal = b;
+				break;
+			}
+		}
+		XYChart.Series series = new XYChart.Series();
+		series.setName("Evolution du solde");
+		
+		double solde = bal.getAmount();
+		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+		int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+		int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+		
+		// Create a calendar object and set year and month
+		Calendar mycal = new GregorianCalendar(currentYear, currentMonth, currentDay);
+		
+		// Get the number of days in that month
+		int daysInMonth = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		
+		if (!bal.getTransactions().isEmpty()) {
+			for (IOTransactionLogic transaction : bal.getTransactions().get(currentYear)[currentMonth]) {
+				solde -= transaction.getAmount();
+			}
+		}
+		
+		for (int i = 0; i < currentDay; ++i) {
+			if (!bal.getTransactions().isEmpty()) {
+				for (IOTransactionLogic transaction : bal.getTransactions().get(currentYear)[currentMonth]) {
+					if (transaction.getDate().getDay() - 1 == i) {
+						solde += transaction.getAmount();
+					}
+				}
+			}
+			
+			series.getData().add(new XYChart.Data(String.valueOf(i + 1), solde));
+		}
+		
+		chartLine.getData().addAll(series);
 	}
 	
 	private void initNodeListe() {
 		
 		btnAddTransaction = new JFXButton("+");
 		btnAddTransaction.setButtonType(JFXButton.ButtonType.RAISED);
-		btnAddTransaction.getStyleClass().addAll("transaction-animate-button", "transaction-animate-button-sub");
+		btnAddTransaction.getStyleClass().addAll("dashboard-animate-button", "dashboard-animate-button-sub");
 		btnOutgo = new JFXButton("Dep");
 		btnOutgo.setButtonType(JFXButton.ButtonType.RAISED);
-		btnOutgo.getStyleClass().addAll("transaction-animate-button", "transaction-animate-button-sub-2");
+		btnOutgo.getStyleClass().addAll("dashboard-animate-button", "dashboard-animate-button-sub-2");
 		btnIncome = new JFXButton("Rev");
 		btnIncome.setButtonType(JFXButton.ButtonType.RAISED);
-		btnIncome.getStyleClass().addAll("transaction-animate-button", "transaction-animate-button-sub-2");
+		btnIncome.getStyleClass().addAll("dashboard-animate-button", "dashboard-animate-button-sub-2");
 		
 		nodeAjout.addAnimatedNode(btnAddTransaction);
 		nodeAjout.addAnimatedNode(btnOutgo);
 		nodeAjout.addAnimatedNode(btnIncome);
 		nodeAjout.setSpacing(5d);
 		nodeAjout.setRotate(180);
-		
 		
 	}
 }
