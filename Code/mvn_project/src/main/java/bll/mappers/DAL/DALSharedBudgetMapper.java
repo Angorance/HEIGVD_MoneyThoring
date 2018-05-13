51d76c4be5357fba9d16e7bfa4f8082d248f5d61
Helena <helena.line.reymond@gmail.com>
2018-05-13 23:12:40 +0200

    DALCategoryBudgetMapper implemented

---
+++ Code/mvn_project/src/main/java/bll/mappers/DAL/DALSharedBudgetMapper.java
@@ -0,0 +1,9 @@
+package bll.mappers.DAL;
+
+/**
+ * Class used to map a SharedBudgetModel to an IDALSharedbudgetEntity
+ *
+ * @author Héléna Line Reymond
+ * @version 1.0
+ */
+public class DALSharedBudgetMapper {}
